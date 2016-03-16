39fd7632411435e0b4c63a1e1d263c74e736881c
Michael Bernard Bryan D <mddoron@up.edu.ph>
2016-03-16 09:36:27 +0000

    Parser edit

--- projectargs/parser.py
+++ projectargs/parser.py
@@ -3,7 +3,16 @@
 class Parser:
     def __init__(self):
         print("Hello World")
+     
+    @staticmethod
+    def fileread(filename):
+        file = open(filename, 'r', 1)
+        print(file);
         
-test = Parser()
+        filebuf = file.read();
+        
+        return filebuf
 
-    
+filebuf = Parser.fileread("../schedule-list/classlist-2015-1.csv");
+
+print(filebuf);
